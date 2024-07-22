9a44530593c3a55a35fdee8b26d16528ec6dde50
madguy9x <tinhnguyen21.edu@gmail.com>
2024-07-22 16:28:55 +0700

    Test build

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ const thisYear = new Date().getFullYear()
     />
     <div class="container relative max-w-2xl mx-auto bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5">
       <header class="px-4 pt-6 prose-sm md:px-6 md:prose">
-        <h1>Vite + Vue 3 + TypeScript + Tailwind + Playwright Starter Template v{{ VERSION }}</h1>
+        <h1>Updated Vite + Vue 3 + TypeScript + Tailwind + Playwright Starter Template v{{ VERSION }}</h1>
         <p class="pb-4 text-xl leading-relaxed tracking-wide text-gray-700">
           Opinionated, production ready template for Vite and Vue 3. MIT licensed,
           <a href="https://github.com/Uninen/vite-ts-tailwind-starter">available on GitHub</a>.
